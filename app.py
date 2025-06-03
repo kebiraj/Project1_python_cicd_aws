a5bc91593b6933178b658f7a00e53dd111f38a82
Kebiraj S <kebiraj@gmail.com>
2025-06-03 18:36:50 +0530

    Update app.py
    hia

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Team, welcome and it's my application from Flask in Docker on EC2- by Kebiraj!"
+    return "Hello Hia, welcome and it's my application from Flask in Docker on EC2- by Kebiraj!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
